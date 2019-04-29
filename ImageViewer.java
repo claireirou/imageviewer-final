be21230a4b7b2a3942805a6dc04de21afeaee028
Claire Irou <c.iroudayassamy@gmail.com>
2019-04-29 10:41:20 -0400

    Created Channel Filters
    Created and coded the three classes for Red, green, blue channel filter.
    In each class, the apply() method gets a pixel, gets the red/blue/green value of that pixel and then reassigns the RGB values to that value.

--- ImageViewer.java
+++ ImageViewer.java
@@ -255,6 +255,9 @@ public class ImageViewer
         filterList.add(new GrayScaleFilter("Grayscale"));
         filterList.add(new EdgeFilter("Edge Detection"));
         filterList.add(new FishEyeFilter("Fish Eye"));
+        filterList.add(new RedChannelFilter("Red Channel"));
+        filterList.add(new BlueChannelFilter("Blue Channel"));
+        filterList.add(new GreenChannelFilter("Green Channel"));
        
         return filterList;
     }
