29872b4cab319347b26bf61b28ab11a76807341e
Claire Irou <c.iroudayassamy@gmail.com>
2019-05-01 17:57:36 -0400

    Logic, what's that? Lab 13 Finished
    I think aliens took over my brain during class, because I can't figure out how I ever thought what I was doing would work. I've now gotten a good night's sleep and re-tackled the Warhol filter.
    First up, basic math. To make an image 1/4 it's original size you halve its width and height, you do not quarter both of them. What was I thinking? Only the aliens know. That's fixed now.
    I was really hoping to be able to call the Color Channel Filters to use in the Warhol filter, but if there's a clean, effecient way to do that, I'm not seeing it, so eveything is handled by the Warhol filter. It redraws the image smaller and four times over to make the four quadrants. Then each pixel in each quadrant is recolored accordingly.
    Lab 13 Completed.

--- WarholFilter.java
+++ WarholFilter.java
@@ -2,23 +2,18 @@ import java.awt.Color;
 
 /**
  * An image filter to show a Warhol inspired version of the image.
+ * The image is split up into four quadrants each with a different
+ * filter on it.
  *
  * @author Claire Iroudayassamy
  * @version 2019.04.29
  */
 public class WarholFilter extends Filter
 {
-    
     private OFImage original;
-    private OFImage quadrant1;
-    private OFImage quadrant2;
-    private OFImage quadrant3;
-    private OFImage quadrant4;
-    private int width;
-    private int height;
-    private RedChannelFilter redChannel;
-    private GreenChannelFilter greenChannel;
-    private BlueChannelFilter blueChannel;
+    private OFImage newImage;
+    private int newHeight;
+    private int newWidth;
     
     /**
      * Constructor for objects of class WarholFilter
@@ -30,46 +25,74 @@ public class WarholFilter extends Filter
     }
 
     /**
-     * Apply this filter to an image
+     * Apply this filter to an image.
      * 
      * @param image The image to be changed by this filter
      */
     public void apply(OFImage image)
     {
         original = new OFImage(image);
-        width = original.getWidth();
-        height = original.getHeight();
+        newWidth = original.getWidth() / 2;
+        newHeight = original.getHeight() / 2;
         
-        quadrant1 = resize(image);
-        quadrant2 = resize(image);
-        quadrant3 = resize(image);
-        quadrant4 = resize(image);
-        
-        redChannel.apply(quadrant1);
-        greenChannel.apply(quadrant3);
-        blueChannel.apply(quadrant4);
-        
-        
-        
-    }
-     
-    /**
-     * Resize the image to 1/4 its original size.
-     * 
-     * @param image The image to be resized.
-     */
-    private OFImage resize(OFImage image)
-    {
-        int width = image.getWidth() / 4;
-        int height = image.getHeight() / 4;
-        OFImage newImage = new OFImage(width, height);
-        
-        for(int y = 0; y < height; y++) {
-            for(int x = 0; x < width; x++) {
-                newImage.setPixel(x, y, image.getPixel(x * 4, y * 4));
+        // Display quadrant I.
+        for(int y = 0; y < newHeight; y++) {
+            for(int x = 0; x < newWidth; x++) {
+                image.setPixel(x + newWidth, y, original.getPixel(x * 2, y * 2));
             }
         }
         
-        return newImage;
+        // Display quadrant II.
+        for(int y = 0; y < newHeight; y++) {
+            for(int x = 0; x < newWidth; x++) {
+                image.setPixel(x, y, original.getPixel(x * 2, y * 2));
+            }
+        }
+        
+        // Display quadrant III.
+        for(int y = 0; y < newHeight; y++) {
+            for(int x = 0; x < newWidth; x++) {
+                image.setPixel(x, y + newHeight, original.getPixel(x * 2, y * 2));
+            }
+        }
+        
+        // Display quadrant IV.
+        for(int y = 0; y < newHeight; y++) {
+            for(int x = 0; x < newWidth; x++) {
+                image.setPixel(x + newWidth, y + newHeight, original.getPixel(x * 2, y * 2));
+            }
+        }
+        
+        
+        // Set quadrant filters
+        // Quadrant I Red Channel Filter
+        for(int y = 0; y < newHeight; y++) {
+            for(int x = 0; x < newWidth; x++) {
+                Color pix = image.getPixel(x, y);
+                int redValue = pix.getRed();
+                
+                image.setPixel(x + newWidth, y, new Color(redValue, redValue, redValue));
+            }
+        }
+        
+        // Quadrant III Green Channel Filter
+        for(int y = 0; y < newHeight; y++) {
+            for(int x = 0; x < newWidth; x++) {
+                Color pix = image.getPixel(x, y);
+                int greenValue = pix.getGreen();
+                
+                image.setPixel(x, y + newHeight, new Color(greenValue, greenValue, greenValue));
+            }
+        }
+        
+        // Quadrant IV Blue Channel Filter
+        for(int y = 0; y < newHeight; y++) {
+            for(int x = 0; x < newWidth; x++) {
+                Color pix = image.getPixel(x, y);
+                int blueValue = pix.getBlue();
+                
+                image.setPixel(x + newWidth, y + newHeight, new Color(blueValue, blueValue, blueValue));
+            }
+        }
     }
 }
